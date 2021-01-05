d447aa4114fbcc15be163e8b2634c4a4ee0e0a03
Suppa <joshsuppa@gmail.com>
2021-01-04 22:00:03 -0500

    my changes

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-console.log('Hello World');
+console.log("Hello World");
 
 function add(a, b) {
   return a + b;
 }
 
-console.log('new Code');
+console.log("new Code");
 
 function sayHi(msg) {
   console.log(msg);
 }
 
-console.log('Kate was hereeeeee :D');
+console.log("Kate was hereeeeee :D");
 
-console.log('Kyle Was HERE SUCKAS');
+console.log("Kyle Was HERE SUCKAS");
+console.log("Suppa Was HERE SUCKAS");
