8597ba866bbe9886fb0e2b0a04a05363a8f9c338
Kaitlin Bray <Kaitlin.bray@yahoo.com>
2021-01-04 21:43:36 -0500

    add kate

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 console.log('Hello World');
 
-
-
-function add(a,b) {
-    return a+b;
+function add(a, b) {
+  return a + b;
 }
 
 console.log('new Code');
 
 function sayHi(msg) {
-    console.log(msg);
+  console.log(msg);
 }
+
+console.log('Kate was hereeeeee :D');
